68279018803455cc0d918d73b0a92831c30d7261
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-11-08 11:08:05 -0500

    creating the first model for credential, might add swagger

---
+++ rhaxx-credential-service/src/main/java/com/rhaxx/rhaxxcredentialservice/models/Credential.java
@@ -0,0 +1,25 @@
+package com.rhaxx.rhaxxcredentialservice.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CREDENTIAL")
+public class Credential {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CREDENTIAL_ID_SEQ")
+    @SequenceGenerator(name = "CREDENTIAL_ID_SEQ", sequenceName = "CREDENTIAL_ID_SEQ", allocationSize = 1, initialValue = 1)
+    @Column(name = "CREDENTIAL_ID")
+    private long credentialId;
+    @Column(name = "CREDENTIAL_USERNAME")
+    private String username;
+    @Column(name = "CREDENTIAL_PASSWORD")
+    private String password;
+
+}
