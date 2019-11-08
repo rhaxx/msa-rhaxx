d614f2909a48295110f142f0f6897e585cf4a744
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-11-08 12:53:36 -0500

    moving things around

---
+++ rhaxx-authentication-service/src/test/java/com/rhaxx/rhaxxauthenticationservice/RhaxxAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhaxx.rhaxxauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhaxxAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- rhaxx-blah-service/src/main/java/com/rhaxx/rhaxxauthenticationservice/RhaxxAuthenticationApplication.java
+++ rhaxx-blah-service/src/main/java/com/rhaxx/rhaxxauthenticationservice/RhaxxAuthenticationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RhaxxAuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RhaxxAuthenticationApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
