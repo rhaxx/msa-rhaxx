1e67f2af88bd2f3d5fcaf6c997bc80835f88b3b6
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-11-08 13:20:03 -0500

    creating the new authentication services

--- rhaxx-authentication-service/src/main/java/com/rhaxx/rhaxxauthenticationservice/AuthenticationApplication.java
+++ rhaxx-authentication-service/src/main/java/com/rhaxx/rhaxxauthenticationservice/AuthenticationApplication.java
@@ -2,12 +2,14 @@ package com.rhaxx.rhaxxauthenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class RhaxxAuthenticationApplication {
+public class AuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RhaxxAuthenticationApplication.class, args);
+		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
 }
