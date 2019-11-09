a3d2ce0e3dbb2670e59861ebb03ba65c8bd9cd07
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-11-08 21:40:01 -0500

    combined the services into one and the controllers into one

---
+++ rhaxx-authentication-service/src/main/java/com/rhaxx/rhaxxauthenticationservice/controllers/AuthenticationController.java
@@ -0,0 +1,63 @@
+package com.rhaxx.rhaxxauthenticationservice.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.rhaxx.rhaxxauthenticationservice.models.Credential;
+import com.rhaxx.rhaxxauthenticationservice.models.Player;
+import com.rhaxx.rhaxxauthenticationservice.services.AuthenticationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/authentication")
+public class AuthenticationController {
+
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @GetMapping(value = "/credential/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Credential>> getAllCredentials() {
+        List<Credential> credentials = authenticationService.getAllCredentials();
+        return new ResponseEntity<>(credentials, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/credential/create", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Credential> createCredential(@Valid @RequestBody Credential credential) {
+        if (credential != null) {
+            return new ResponseEntity<>(this.authenticationService.createCredential(credential), HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(value = "/player/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Player>> getAllPlayers() {
+		List<Player> players = authenticationService.getAllPlayers();
+		return new ResponseEntity<>(players, HttpStatus.OK);
+	}
+
+    @PostMapping(value = "/player/create", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Player> createPlayer(@Valid @RequestBody Player player) {
+        if(player != null) {
+            return new ResponseEntity<>(this.authenticationService.createPlayer(player), HttpStatus.CREATED);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
