ae52282531e54bfcc0e86e3d2f79ba8b583f4ca5
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-11-11 10:40:04 -0500

    fixed the GatewayApplication class to start up properly

--- rhaxx-gateway-service/src/main/java/com/rhaxx/rhaxxgatewayservice/GatewayApplication.java
+++ rhaxx-gateway-service/src/main/java/com/rhaxx/rhaxxgatewayservice/GatewayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
