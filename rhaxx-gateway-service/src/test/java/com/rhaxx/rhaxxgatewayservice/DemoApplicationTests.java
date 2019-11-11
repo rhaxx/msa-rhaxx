cb51b40a8f21a414f40e7ddef6109b3d67fe2621
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-11-11 10:26:01 -0500

    creating the gateway service

---
+++ rhaxx-gateway-service/src/test/java/com/rhaxx/rhaxxgatewayservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhaxx.rhaxxgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
